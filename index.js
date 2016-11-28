9a60f3e13389bc151d057f51841bcfe6b8070190
i5ting <i5ting@126.com>
2016-11-28 23:05:37 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ var fs = require('fs')
 var gulp = require('gulp')
 
 module.exports = function (opts) {
-  var conf = require(opts.cwd + '/package.json')
-  var all = require(opts.root + '/static/task.json')
+  try{
+    var conf = require(opts.cwd + '/package.json')
+    var all = require(opts.root + '/static/task.json')
 
-  require(opts.cwd + '/gulpfile')
+    require(opts.cwd + '/gulpfile')
 
-  var gulpTasks = Object.keys(gulp.tasks);
+    var gulpTasks = Object.keys(gulp.tasks);
   
-  console.log('available gulp task:')
-  console.log(gulpTasks);
+    console.log('available gulp task:')
+    console.log(gulpTasks);
   
   
-  all.tasks.gulp = {
-    prefix: 'gulp',
-    tasks: gulpTasks
+    all.tasks.gulp = {
+      prefix: 'gulp',
+      tasks: gulpTasks
+    }
+
+    fs.writeFileSync(opts.root + '/static/task.json',  JSON.stringify(all, null, 4))
   }
-
-  fs.writeFileSync(opts.root + '/static/task.json',  JSON.stringify(all, null, 4))
+  catch(err)
+  {
+    //在此处理错误
+    console.log(err)
+  }
 }
